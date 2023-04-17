ba6babde3f1a866b986f11e30cf6d353b9ff90ab
Antoni Krzyżaniak <73665@ue.poznan.pl>
2023-04-17 22:27:22 +0200

    added zadanie 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -32,3 +32,10 @@ potega = 4
 wynik = pow(liczba,potega)
 
 print("Wynik wynosi: ",wynik)
+
+# zadanie 1.6
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
