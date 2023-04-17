74978b93cadfdeef47935f224a93ca0e610ebdb8
Antoni Krzyżaniak <73665@ue.poznan.pl>
2023-04-17 22:32:59 +0200

    added zadanie 1.7

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,26 +1,26 @@
 # zadanie 1.1
+
 hello = "Hello"
 student = "Ola"
-# oczekiwany rezultat: Hello Ola
-# wykorzystaj w princie zmienne hello i student
+
 print("{} {}".format(hello,student))
 
 # zadanie 1.2
+
 student = input("Wpisz swoje imie: ")
 print("Hello "+student)
 
-# zadanie 1.3 - policz liczbe studentow w tablicy studenci 
+# zadanie 1.3
+
 studenci = ["Ania", "Kuba", "Piotr", "Jan"]
 
-# policz liczbe studentow w tablicy studenci 
-# oczekiwany rezultat: Liczba studentow wynosi: 4
 liczba_studentow = len(studenci)
 print("Liczba studentow wynosi: ", liczba_studentow)
 
-# zadanie 1.4 - za pomoca petli i print przywitaj sie z kazdym studentem z tabeli studenci osobno
+# zadanie 1.4
+
 studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
 
-# za pomoca petli i print przywitaj sie z kazdym studentem
 for x in range(liczba_studentow):
     print("Hello "+studenci[x])
 
@@ -39,3 +39,12 @@ ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
 liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
 
 print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
+
+# zadanie 1.7
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci.sort()
+
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
