51d1c32600ed48851d8820ffb009dda6ed62ba39
Antoni Krzyzaniak <marvtheminer@gmail.com>
2023-05-15 12:12:57 +0200

    added solo excercise

--- solo-work/solo_2.py
+++ solo-work/solo_2.py
@@ -1,3 +1,6 @@
+from cmath import pi
+
+
 print("buongiorno")
 
 class Trojkat:
@@ -14,8 +17,23 @@ class Trojkat:
     def pole(self):
         return self.bok_a * self.h_a * 0.5
 
+class Kolo:
+    def __init__(self, r):
+        self.promien = r
+
+    def obwod(self):
+        return self.promien * pi * 2
+
+    def pole(self):
+        return self.promien * self.promien * pi
+
+kolo_o_promieniu_6 = Kolo(6)
+
 trojkat_rownoboczny = Trojkat(10, 10, 10, 8)
 trojkat_antka = Trojkat(12, 13, 5, 12)
 
 print(trojkat_rownoboczny.obwod())
 print(trojkat_antka.pole())
+
+print(kolo_o_promieniu_6.obwod())
+print(kolo_o_promieniu_6.pole())
