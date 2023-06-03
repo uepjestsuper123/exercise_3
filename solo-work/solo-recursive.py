062eb6a80b9f5e17b82c4c333d1c2c8d8ebcb31d
Antoni Krzyżaniak <73665@ue.poznan.pl>
2023-06-03 12:23:34 +0200

    added sudoku 2x2

--- solo-work/solo-recursive.py
+++ solo-work/solo-recursive.py
@@ -39,9 +39,9 @@ print(f"The factorial of {number} is: {n_factorial}")
 
 def fibonacci(n):
     if n <= 0:
-        return [] #dla n=0 zwracamy pustą listę
+        return [0] #dla n=0 zwracamy jednoelementową listę: [0]
     elif n == 1:
-        return [0, 1] # dla n=1 zwracamy jednoelementową listę
+        return [0, 1] # dla n=1 zwracamy dwuelementową listę: [0, 1]
     else:
         sequence = fibonacci(n - 1)
         sequence.append(sequence[-1] + sequence[-2])
@@ -49,4 +49,41 @@ def fibonacci(n):
 
 number2 = 5
 fibo = fibonacci(number2)
-print(f"The Fibonacci sequence {number2} is: {fibo}")
+print(f"The Fibonacci sequence for {number2} is: {fibo}")
+
+def solve_sudoku(grid):
+    for i in range(2): 
+        for j in range(2): #iterowanie przez kolumny i wiersze
+            if grid[i][j] == 0: #sprawdzanie czy komórki sa puste
+                for num in range(1, 3): #jeśli tak, to próbujemy je wypełnić liczbami od 1 do 2
+                    if is_valid_move(grid, i, j, num): #sprawdzamy czy możemy wstawić daną liczbę
+                        grid[i][j] = num #jeśli tak to wstawiamy
+                        if solve_sudoku(grid): #wracając wypełniamy resztę komórek
+                            return True
+                        grid[i][j] = 0
+                return False #jeśli nie ma możliwości dodania liczby to niepowodzenie
+    return True #w przeciwnym razie jest super!!! :)
+
+def is_valid_move(grid, row, col, num): #sprawdzamy czy w kolumnach lub wierszach liczby się nie powtarzają
+    for i in range(2):
+        if grid[row][i] == num or grid[i][col] == num:
+            return False
+    return True
+
+sudoku_grid = [[2, 0], [1, 0]]
+
+if solve_sudoku(sudoku_grid):
+    print("Sudoku solved:")
+    for row in sudoku_grid:
+        print(*row)
+else:
+    print("No solution exists for the Sudoku.")
+
+sudoku_grid2 = [[2, 0], [0, 1]]
+
+if solve_sudoku(sudoku_grid2):
+    print("Sudoku solved:")
+    for row in sudoku_grid2:
+        print(*row)
+else:
+    print("No solution exists for the Sudoku.")
